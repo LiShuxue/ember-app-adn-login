8fe94a9d601b701cd4ab99b1ed33eab5e395b513
LiShuxue <1149926505@qq.com>
2017-11-09 17:31:21 +0800

    前几次在这个addon中测试组件的方法有问题，不应该那样测试。首先删掉那些文件。然后改下dummy/app/app.js和dummy/app/router.js。为什么改这两个，因为真正在application中应用这个addon的时候就是这样的。详情参考https://github.com/LiShuxue/ember-app.git

--- tests/dummy/app/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-    actions: {
-        loginAction() {
-            Ember.Logger.log('login success');
-        }
-    }
-});
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login');
+  this.mount('ember-app-adn-login', {path: '/adn-login'});
 });
 
 export default Router;
