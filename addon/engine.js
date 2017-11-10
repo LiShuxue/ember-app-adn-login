872dbb1725f68fad1896111e584e6fc4629c04a0
LiShuxue <1149926505@qq.com>
2017-11-10 16:16:21 +0800

    在addon engine中通过services来指定engine需要用到的service，这些service需要宿主应用去提供。宿主应用详情参考：https://github.com/LiShuxue/ember-app.git提交记录

--- addon/engine.js
+++ addon/engine.js
@@ -5,11 +5,14 @@ import config from './config/environment';
  
 const { modulePrefix } = config;
 // externalRoutes: 指定engine需要跳转的外部链接。Engine defines what it would like to go to and the application tells it where that is.
+// services: 指定engine需要用到的service，这些service需要宿主应用去提供。Your Engine will expect the host application to provide service and it will uses. 
 const Eng = Engine.extend({
   modulePrefix,
   Resolver,
   dependencies: {
-    services: [],
+    services: [
+      'logService'
+    ],
     externalRoutes: [
       'test'
     ]
--- addon/routes/login.js
+++ addon/routes/login.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    logService: Ember.inject.service(),
     actions: {
         testGo() {
             this.transitionToExternal('test');
+        },
+        testServiceFromEngine() {
+            this.get('logService').log();
         }
     }
 });
