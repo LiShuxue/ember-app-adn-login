3107cd482db83e662aacd069c61ed951500a82e2
LiShuxue <1149926505@qq.com>
2017-11-10 15:53:09 +0800

    在addon engine中通过externalRoutes来指定可以跳转的外部链接，而在宿主应用中也需要指定那个链接是什么。宿主应用详情参考：https://github.com/LiShuxue/ember-app.git提交记录

--- addon/engine.js
+++ addon/engine.js
@@ -4,13 +4,15 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
  
 const { modulePrefix } = config;
- 
+// externalRoutes: 指定engine需要跳转的外部链接。Engine defines what it would like to go to and the application tells it where that is.
 const Eng = Engine.extend({
   modulePrefix,
   Resolver,
   dependencies: {
     services: [],
-    externalRoutes: []
+    externalRoutes: [
+      'test'
+    ]
   }
 });
  
--- addon/routes/login.js
+++ addon/routes/login.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    actions: {
+        testGo() {
+            this.transitionToExternal('test');
+        }
+    }
 });
