f9948df2ad629c7637ab255016ff2ce606f416e7
LiShuxue <1149926505@qq.com>
2018-02-12 17:22:34 +0800

    使用ember-burger-menu来创建菜单，注意要引入正确的属性，以及修改.ember-burger-menu样式才能生效。

---
+++ app/templates/navigation.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-app-adn-login/templates/navigation';
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   redirect() {
-    this.transitionTo('ember-app-adn-login.login');
+    this.transitionTo('ember-app-adn-login.navigation');
   }
 });
